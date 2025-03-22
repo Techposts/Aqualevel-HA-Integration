4c4ebe9b61a07a801b8eddccb15225d864de98fa
Ravi Singh <ravi.singh543@gmail.com>
2025-03-22 20:03:44 +0530

    Update config_flow.py

--- custom_components/aqualevel/config_flow.py
+++ custom_components/aqualevel/config_flow.py
@@ -14,7 +14,8 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.selector import SelectSelector, SelectSelectorConfig, SelectSelectorMode
 import async_timeout
 
-from .const import DOMAIN, CONF_HOST
+from .const import DOMAIN
+from homeassistant.const import CONF_HOST, CONF_NAME
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -53,21 +54,40 @@ class AquaLevelConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         if user_input is not None:
             host = user_input[CONF_HOST]
+            name = user_input.get(CONF_NAME)
+            
             if _validate_host(host):
                 await self.async_set_unique_id(host)
                 self._abort_if_unique_id_configured()
-                return self.async_create_entry(title=host, data={CONF_HOST: host})
+                
+                data = {CONF_HOST: host}
+                if name:
+                    data[CONF_NAME] = name
+                    
+                return self.async_create_entry(
+                    title=name or host, 
+                    data=data
+                )
             else:
                 errors[CONF_HOST] = "invalid_host"
 
-        schema = vol.Schema({
-            vol.Required(CONF_HOST): SelectSelector(
-                SelectSelectorConfig(
-                    options=list(self._discovered_devices.keys()),
-                    mode=SelectSelectorMode.DROPDOWN
-                )
-            )
-        })
+        # Different schema based on whether devices were discovered
+        if self._discovered_devices:
+            schema = vol.Schema({
+                vol.Required(CONF_HOST): SelectSelector(
+                    SelectSelectorConfig(
+                        options=list(self._discovered_devices.keys()),
+                        mode=SelectSelectorMode.DROPDOWN
+                    )
+                ),
+                vol.Optional(CONF_NAME): str,
+            })
+        else:
+            # No devices discovered, allow manual entry
+            schema = vol.Schema({
+                vol.Required(CONF_HOST): str,
+                vol.Optional(CONF_NAME): str,
+            })
 
         return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
 
@@ -82,18 +102,47 @@ class AquaLevelConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_create_entry(title=self._host, data={CONF_HOST: self._host})
 
     async def _discover_devices(self) -> Dict[str, str]:
-        """Discover Aqua Level devices on the local network."""
+        """Discover AquaLevel devices on the local network."""
         session = async_get_clientsession(self.hass)
         discovered = {}
-        hostname = "aqua-level"
+        
+        # Try both possible hostname formats
+        hostnames = ["aqua-level", "aqualevel-garden", "aqualevel-main"]
+        
+        for hostname in hostnames:
+            try:
+                addr_info = await self.hass.async_add_executor_job(
+                    socket.gethostbyname, f"{hostname}.local"
+                )
+                url = f"http://{addr_info}/status"
+                if await _try_endpoint(session, url):
+                    discovered[addr_info] = hostname
+            except socket.gaierror:
+                _LOGGER.debug("Could not resolve %s.local", hostname)
+        
+        # Try to scan for devices with aqualevel- prefix
         try:
-            addr_info = await self.hass.async_add_executor_job(socket.gethostbyname, f"{hostname}.local")
-            url = f"http://{addr_info}/status"
-            if await _try_endpoint(session, url):
-                discovered[addr_info] = hostname
-        except socket.gaierror:
-            _LOGGER.warning("Could not resolve %s.local", hostname)
+            import zeroconf
+            browser = zeroconf.ServiceBrowser(
+                zeroconf.Zeroconf(), "_http._tcp.local.",
+                handlers=[self._on_service_state_change]
+            )
+            # Give some time for discovery
+            await asyncio.sleep(3)
+            browser.cancel()
+        except (ImportError, Exception) as err:
+            _LOGGER.debug("Error during mDNS scan: %s", err)
+            
         return discovered
+    
+    def _on_service_state_change(self, zeroconf, service_type, name, state_change):
+        """Handle zeroconf state changes."""
+        if state_change in [zeroconf.ServiceStateChange.Added, zeroconf.ServiceStateChange.Updated]:
+            info = zeroconf.get_service_info(service_type, name)
+            if info and "aqualevel" in name.lower():
+                # Extract IP address
+                for addr in info.parsed_addresses():
+                    self._discovered_devices[addr] = name.split('.')[0]
 
     @staticmethod
     @callback
